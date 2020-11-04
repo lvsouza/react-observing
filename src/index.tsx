e5d1631bfbf72851bd5e0ec71f0e722255793da2
lvsouza <ls18017@gmail.com>
2020-11-03 21:33:19 -0300

    chor: updated package.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
+export type { IListeners, IObservable, ISubscription } from './interfaces'
 export { useObserver, useSetObserver, useObserverValue } from './hooks'
-export { IListeners, IObservable, ISubscription } from './interfaces'
 export { set, observe, isObservableProp } from './core'
