422cf36e628adc1cb7c5b9625ed280d42a6ce098
lvargas <lvargas@gvdasa.com.br>
2021-11-17 18:12:39 -0300

    tests: added tests to subscribe in observable

--- src/core/Observable.test.ts
+++ src/core/Observable.test.ts
@@ -12,4 +12,34 @@ describe('New store', () => {
 
     expect(Store.value).toBe(false);
   });
+
+  it('Add a valid subscription', () => {
+    const subscription = Store.subscribe(() => { })
+
+    expect(subscription.observerId).toBe(Store.id);
+    expect(typeof subscription.observerId).toBe('string');
+    expect(typeof subscription.unsubscribe).toBe('function');
+
+    subscription.unsubscribe();
+  });
+
+  it('Subscribe and listen for value changes as "false"', () => {
+    const subscription = Store.subscribe((value) => {
+      expect(value).toBe(false);
+    });
+
+    Store.value = false;
+
+    subscription.unsubscribe();
+  });
+
+  it('Subscribe and listen for value changes as "true"', () => {
+    const subscription = Store.subscribe((value) => {
+      expect(value).toBe(true);
+    });
+
+    Store.value = true;
+
+    subscription.unsubscribe();
+  });
 });
